31df544f2adcba431f6a5e23ba4b0668346d7079
yayner <yayner2002@gmail.com>
2022-06-03 23:39:30 +0300

    Add first hook setstate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount ] = useState()
+  function handleDec() {
+    setCount(state => state -1)
+  }
+  function incCount() {
+    setCount((count) => count + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleDec}>-</button><span>{count}</span><button onClick={incCount}>+</button>
     </div>
   );
 }
