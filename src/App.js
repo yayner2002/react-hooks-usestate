8605659795fef614529a1338f007cdfea6ad43a1
yayner <yayner2002@gmail.com>
2022-06-04 01:52:28 +0300

    Refactor it to use window width

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [restype, setRes] = useState('posts')
-  const [items, setItem] = useState([])
+  // const [restype, setRes] = useState('posts')
+  // const [items, setItem] = useState([])
+  const [winWidth, setWinWidth] = useState(window.innerWidth)
 
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${restype}`)
-    .then(res => res.json())
-    .then(data => setItem(data))
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${restype}`)
+  //   .then(res => res.json())
+  //   .then(data => setItem(data))
 
-  },[restype])
+  // },[restype])
   // const [count, setCount ] = useState(10)
   // const [theme, setTheme ] = useState('red')
 
@@ -22,17 +23,20 @@ function App() {
 //     setTheme('yay')
 //   }
   return (
-    <React.Fragment>
-      <div className="App">
-      <button onClick={() => setRes('posts')}>Posts</button>
-      <button onClick={() => setRes('users')}>Users</button>
-      <button onClick={() => setRes('comments')}>Comments</button>
+    <div>
+      {winWidth}
     </div>
-    <div>{restype}</div>
-    {items.map(i => {
-      return <pre>{JSON.stringify(i)}</pre>
-    })}
-    </React.Fragment>
+    // <React.Fragment>
+    //   <div className="App">
+    //   <button onClick={() => setRes('posts')}>Posts</button>
+    //   <button onClick={() => setRes('users')}>Users</button>
+    //   <button onClick={() => setRes('comments')}>Comments</button>
+    // </div>
+    // <div>{restype}</div>
+    // {items.map(i => {
+    //   return <pre>{JSON.stringify(i)}</pre>
+    // })}
+    // </React.Fragment>
   );
 }
 
